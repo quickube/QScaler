53f37dded17222bfd583049e289f47b753e32103
ofri rom <ofrirom987@gmail.com>
2024-12-24 02:28:50 -0800

    metrics controller

--- cmd/main.go
+++ cmd/main.go
@@ -157,6 +157,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ScalerConfig")
 		os.Exit(1)
 	}
+
+	if err = (&controller.MetricsControllerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MetricsController")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
